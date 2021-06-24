e5295bf4ab3f1e390170794b760e62add052d241
ultrastem <ultrastem@ultrastem.com>
2021-06-24 15:34:34 +0200

    renamed files

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     url='https://github.com/shabihsherjeel/nionswift_elabftw_plugin',
     #download_url = 'https://github.com/shabihsherjeel/nionswift_elabftw_plugin/archive/v0.1.2.4-alpha.tar.gz',
     keywords = ['NIONSWIFT', 'ELABFTW', 'ELN', 'PLUGIN'],
-    packages=['nionswift_plugin.nionswift_elabftw_plugin'],
-    install_requires=['elabapy', 'cryptography', 'nionutils', 'nionui', 'nionswift'],
+    packages=['nionswift_plugin.nionswift_elabftw_plugin'],#, 'elabapy'],
+    install_requires=['cryptography', 'nionutils', 'nionui', 'nionswift'],#, 'elabapy'],
     classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Science/Research',
